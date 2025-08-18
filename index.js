3ad791e1dbf0d07930d3fe70cc1bdef772b7f51d
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-08-19 00:48:23 +0600

    implemented the  utility in the  file

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 const http = require('http');
 const requestResponseHandler = require('./helpers/requestResponseHandler.js');
 const port = process.env.PORT || 3000;
+const data = require('./lib/data.js');
+
+// test the file system
+data.create('test', 'newFile', {name: 'Omar Bin Saleh', role: 'Frontend Developer'}, (err) => {
+   if (err) {
+      return console.log(err);
+   }
+   
+   console.log("Write operation was successful");
+})
 
 // Create the Server
 const server = http.createServer(requestResponseHandler)
