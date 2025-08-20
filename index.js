cf7188793c2dff616c9f69b464f9eb3f175bd8c7
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-08-21 00:49:57 +0600

    implemented  a method named delete in the './lib/data.js' file and refactored the file to add custome errors

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const requestResponseHandler = require('./helpers/requestResponseHandler.js');
 const port = process.env.PORT || 3000;
 const data = require('./lib/data.js');
 
-// test the file system
-data.update('test', 'newFile', [{name: "omar bin saleh", role: "Frontend developer"}, {name: "fatema rima", role: "react developer"}], (err) => {
-   if (err) return console.log(err);
-
-   console.log("Updated the file successfully");
-})
-
 // Create the Server
 const server = http.createServer(requestResponseHandler)
 
